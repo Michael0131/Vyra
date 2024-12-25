8845d1db655a2f678fee5ed524c63f45f6186fc3
Michael J. <86041862+Michael0131@users.noreply.github.com>
2024-12-24 18:20:56 -0700

    fixed error

--- network/urls.py
+++ network/urls.py
@@ -4,9 +4,6 @@ from . import views
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls), # Default admin interface
-    path('network/', include ('network.urls')),   # Any URL starting with 'network/' will be handled by the urls.py in the 'network' app
-    path('test/', views.test_view, name ="test") # Maps the URL 'network/test/' to the 'test_view' function in views.py
+    path('test/', views.test_view, name="test"),  # Map 'test/' to test_view in views.py
 ]
