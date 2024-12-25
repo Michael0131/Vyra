f386d52ceb30be338a0423b8ff3e9728f9cb518b
Michael J. <86041862+Michael0131@users.noreply.github.com>
2024-12-24 18:17:06 -0700

    i might have set up the django server

---
+++ network/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse  # To send a basic HTTP response
+# Create your views here.
+
+def test_view(request):
+    return HttpResponse("Hellow this is the network app test view") # This is a placeholder view that sends a simple response to test routing
